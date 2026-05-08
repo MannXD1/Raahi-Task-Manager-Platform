574e47549281f57b8331ab9a0e0290a5197f0746
MannXD1 <manishram49211@gmail.com>
2026-05-08 22:24:57 +0530

    Update admin.py

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User, Project, Task
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'role']
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at']
+    filter_horizontal = ['members']
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'project', 'assigned_to', 'status', 'due_date']
+    list_filter = ['status', 'project']
+    fields = ['title', 'description', 'project', 'assigned_to', 'status', 'due_date', 'note', 'attachment']
